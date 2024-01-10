9eaeeabc8e521e19403eb41397e0f4253d3ab315
jlow56 <jeanlougames@hotmail.com>
2024-01-10 09:55:21 +0000

    exercice 4 types et variables

---
+++ types-et-variables/exercice-4.php
@@ -0,0 +1,19 @@
+<?php
+// Créez une variable `$data` qui contient l'entier `42`.
+$data = 42;
+echo($data);
+echo"<br>";
+
+
+// Créez une variable `$float` qui transforme `$data` en float.
+$float = $data ;
+$float = 4.2;
+echo $float;
+echo"<br>";
+
+// Créez une variable `$str` qui transforme `$float` en string.
+$str = $float;
+$str = "4.2 est une chaine de caractères";
+echo $str;
+echo"<br>";
+?>
