629eab2d7d0cfc665b576aae6eff74f2d42920e4
jlow56 <jeanlougames@hotmail.com>
2024-01-10 10:35:53 +0000

    exercice 1 boucles et tableaux

---
+++ boucles-et-tableaux/exercice-1.php
@@ -0,0 +1,21 @@
+<?php
+$animals = ["Chat", "Chien", "Lapin", "Souris"];
+
+for ($i = 0; $i < count($animals); $i++)
+{
+    echo "$animals[$i].<br>\n";
+}
+?>
+
+
+<!--Parcourez le tableau `$animals` et affichez chacun des animaux en sautant une ligne entre chaque.-->
+
+<!--Pour sauter une ligne faites un `echo "<br>";`.-->
+
+<!--### N'oubliez pas de sauvegarder votre travail-->
+
+<!--```sh-->
+<!--git add exercice-1.php-->
+<!--git commit -m "exercice 1 boucles et tableaux"-->
+<!--git push-->
+<!--```-->
