91b9fdfb371e8fdd88e9e825e9be45fe9322924c
jlow56 <jeanlougames@hotmail.com>
2024-01-10 09:42:23 +0000

    exercice 2 types et variables

---
+++ types-et-variables/exercice-2.php
@@ -0,0 +1,10 @@
+<?php
+
+// Déclarez un tableau de nombres appelé `$nb_tab` qui contient des `int` et des `float` et affichez-le en utilisant `print_r`.
+$nb_tab = [2,4,6,2.4,4.2,6.4];
+print_r($nb_tab);
+echo"<br>";
+// Déclarez un tableau de nombres appelé `$str_tab` qui contient des `string` et affichez-le en utilisant `print_r`.
+$str_tab = ['jl','mom','lylou'];
+print_r($str_tab);
+?>
