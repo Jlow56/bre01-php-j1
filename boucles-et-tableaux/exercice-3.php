b49b3f9ca54dd5dd7c49e1fbe057612bf9e3c000
jlow56 <jeanlougames@hotmail.com>
2024-01-10 10:57:08 +0000

    exercice 3 boucles et tableaux

---
+++ boucles-et-tableaux/exercice-3.php
@@ -0,0 +1,19 @@
+<?php
+
+$user = [
+	"firstName" => "Barack",
+	"lastName" => "Obama"
+];
+
+echo "je suis"." ".$user["firstName"]." ".$user["lastName"].",le 44ème président des USA.";
+?>
+
+<!--Utilisez l'injection de variable et le tableau associatif `$user` pour afficher une phrase qui dit `Je suis Barack Obama, le 44ème président des USA.`.-->
+
+<!--### N'oubliez pas de sauvegarder votre travail-->
+
+<!--```sh-->
+<!--git add exercice-3.php-->
+<!--git commit -m "exercice 3 boucles et tableaux"-->
+<!--git push-->
+<!--```-->
