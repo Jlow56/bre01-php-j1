1f6f24ad886e3c1010b911fd29a2360915552fc5
jlow56 <jeanlougames@hotmail.com>
2024-01-10 11:17:30 +0000

    exercice 4 boucles et tableaux

---
+++ boucles-et-tableaux/exercice-4.php
@@ -0,0 +1,32 @@
+<?php
+
+$users = [
+	[
+		"firstName" => "Mari",
+		"lastName" => "Doucet"
+	],
+	[
+		"firstName" => "Hugues",
+		"lastName" => "Froger"
+	]
+];
+
+for ($i =0; $i < count($users); $i ++)
+{
+    echo $users[$i]["firstName"] . " " . $users[$i]["lastName"] . "<br>";
+
+}
+
+?>
+
+
+<!--Parcourez le tableau `users` et affichez le nom et prénom de chacun des utilisateurs en sautant une ligne entre chaque utilisateur.-->
+
+<!--Pour sauter une ligne faites un `echo "<br>";`.-->
+
+<!--### N'oubliez pas de sauvegarder votre travail-->
+
+<!--```sh-->
+<!--git add exercice-4.php-->
+<!--git commit -m "exercice 4 boucles et tableaux"-->
+<!--git push-->
